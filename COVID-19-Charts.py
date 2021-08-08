ff06774eb27996fb7c131195c508b9a47870773e
Fernando Dilland Mireles Cisneros <fernandodilland@gmail.com>
2021-08-07 23:17:32 -0500

    Update

--- COVID-19-Charts.py
+++ COVID-19-Charts.py
@@ -1,7 +1,20 @@
 import pandas as pd
-import plotly.express as px
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv')
+df_nat = pd.read_csv('mx.csv')
 
-fig = px.line(df, x = 'AAPL_x', y = 'AAPL_y', title='Apple Share Prices over time (2014)')
-fig.show()
+df_nat.date = pd.to_datetime(df_nat.date)
+df_nat.cases = pd.to_numeric(df_nat.cases)
+df_nat['new_cases'] = df_nat.cases.diff()
+df_nat['growth_new_cases'] = df_nat.new_cases.diff()
+
+sns.set(rc={'figure.figsize':(11.7,6.27)})
+sns.lineplot(
+    x = 'date',
+    y ='new_cases',
+    data = df_nat)
+
+plt.show()
